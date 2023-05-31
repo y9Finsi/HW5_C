876b5700a104d12c9700372e65d3e0f9a98a0cd2
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-31 13:55:52 +0300

    Update 2.c
    fxd err

--- 2.c
+++ 2.c
@@ -1,1 +1,31 @@
 
+#include <stdio.h>
+
+struct Person {
+    int age;
+    char name[100];
+};
+
+float srednii_vozrast(struct Person ludi[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += ludi[i].age;
+    }
+    return (float)sum / n;
+}
+
+int main() {
+    struct Person ludi[7] = {
+        {17, "Богдан"},
+        {17, "Федя"},
+        {16, "Тима"},
+        {17, "Аня"},
+        {18, "Полшишкин"},
+        {17, "Матюха"},
+        {176, "Леша"}
+        
+    };
+    float srednii = srednii_vozrast(ludi, 3);
+    printf("Srednii vozrast: %f\n", srednii);
+    return 0;
+}
