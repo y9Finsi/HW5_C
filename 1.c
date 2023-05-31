09bdace878a66baa7030707aa15476364aa52a97
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-31 13:39:34 +0300

    Update 1.c
    fxd err

--- 1.c
+++ 1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int razmer, i;
+
+    // вводим размер массива
+    printf("введите размер массива: ");
+    scanf("%d", &razmer);
+
+    // создаем массив и заполняем его данными
+    int array[razmer];
+    for (i = 0; i < razmer; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    // меняем местами элементы в массиве до середины
+    for (i = 0; i < razmer / 2; i++) {
+        int musor = array[i];
+        array[i] = array[razmer - i - 1];
+        array[razmer - i - 1] = musor;
+    }
+
+    // выводим измененный массив
+    printf("поменяный местами массивик: ");
+    for (i = 0; i < razmer; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
