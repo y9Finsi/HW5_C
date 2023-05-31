078b5467935976df38451ac99450afd00fedf2cc
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-31 15:34:12 +0300

    Update 4.c
    fx err

--- 4.c
+++ 4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int nod(int a, int b) {
+return b == 0 ? a : nod(b, a % b);
+}
+
+void reduce_fraction(int *a, int *b) {
+int nod2 = nod(*a, *b);
+
+*a /= nod2;
+*b /= nod2;
+}
+
+int main() {
+int a = 14, b = 49;
+
+reduce_fraction(&a, &b);
+
+printf("%d / %d", a, b);
+
+return 0;
+}
