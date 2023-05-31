7baa707822a5109e70671272e5bbf02d1c50ef43
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-05-31 15:44:44 +0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+
+void abs_array(int *array, int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (*(array+i) < 0) {
+            *(array+i) = -*(array+i);
+            count++;
+        }
+    }
+    printf("%d ", count);
+}
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+    abs_array(array, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+}
