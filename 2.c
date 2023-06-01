15b00410a323d4c6b795cce8d4926ef69b2a084e
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-06-02 01:25:04 +0300

    Новые ошибки и фикс багов
    фикс сломанного массива из-за отсутвующей запятой, фикс старого бага с выводом на float, когда вывод вычесления был первых 3 людей

--- 2.c
+++ 2.c
@@ -9,27 +9,29 @@ struct Person {
 float srednii_vozrast(struct Person ludi[], int n) {
     int sum = 0;
     for (int i = 0; i < n; i++) {
-        sum += ludi[i].age;
+        sum += ludi[i].age;  // Вычисляем сумму возрастов всех людей в массиве ludi
     }
-    return (float)sum / n;
+    return (float)sum / n; // и возвращаем средний возраст всех людей в массиве ludi
+
 }
 
 int main() {
-    struct Person ludi[10] = {
+    // Создаем массив с данными о людишках (студентах сириуса)
+    struct Person ludi[11] = {
         {17, "Богдан"},
         {17, "Федя"},
         {16, "Тима"},
         {17, "Аня"},
         {18, "Полшишкин"},
         {17, "Матюха"},
-        {176, "Леша"}
+        {176, "Леша"},
         {16, "Полина"},
         {17, "Дима"},
-        {16, "Егор"}
-        
-        
+        {16, "Егор"},
+        {16, "Мукасяков"}
     };
-    float srednii = srednii_vozrast(ludi, 3);
-    printf("Srednii vozrast: %f\n", srednii);
+    
+    float srednii = srednii_vozrast(ludi, 11); // Вызываем функцию srednii_vozrast и передаем ей массив ludi и его размер. Результат функции сохраняем в переменной srednii
+    printf("Srednii vozrast: %f\n", srednii); // Выводим на экран средний возраст всех людей в массиве ludi
     return 0;
 }
